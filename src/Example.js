e3912053e7bc9a057adec1e1c51eac7618835f4f
Steve <ycsteven.tsao@gmail.com>
2020-03-05 12:18:27 +0800

    useEffect

--- src/Example.js
+++ src/Example.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+const Index = () => {
+    useEffect(() => {
+        console.log('userEffect=>你來了！Index 頁面');
+        return () => {
+            console.log('你走了！Index 頁面');
+        };
+    }, []);
+    return <h2>JSPang.com</h2>;
+};
+
+const List = () => {
+    useEffect(() => {
+        console.log('userEffect=>List 頁面');
+    });
+    return <h2>List-Page</h2>;
+};
 const Example = () => {
     const [count, setCount] = useState(0);
     useEffect(() => {
@@ -15,6 +32,19 @@ const Example = () => {
             >
                 Click me
             </button>
+            <Router>
+                <ul>
+                    <li>
+                        {' '}
+                        <Link to="/">首页</Link>{' '}
+                    </li>
+                    <li>
+                        <Link to="/list/">列表</Link>{' '}
+                    </li>
+                </ul>
+                <Route path="/" exact component={Index} />
+                <Route path="/list/" component={List} />
+            </Router>
         </div>
     );
 };
